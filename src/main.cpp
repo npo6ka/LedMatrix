af09afb34bd0889e40d5d4ee8a4541afa68b4c15
npo6ka <npo6ka100@gmail.com>
2019-12-11 00:31:05 +0300

    add button processing and auto mode

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@
 // инициализация светодиодов
 CRGB leds[LEDS_CNT];
 
+#define BTN_PIN               (4U)      // пин кнопки (D2)
+#define BUTTON_STEP_TIMEOUT   (100U)    // каждые BUTTON_STEP_TIMEOUT мс будет генерироваться событие удержания кнопки (для регулировки яркости)
+#define BUTTON_CLICK_TIMEOUT  (500U)    // максимальное время между нажатиями кнопки в мс, до достижения которого считается серия последовательных нажатий
+
+#define AUTOMOD_INTERVAL      (5000U)    // кол-во времни между автоматическим переключением режима
+
 EffectsList *effects;
+GButton touch(BTN_PIN, LOW_PULL, NORM_OPEN);
 
 void setup() {
     randomSeed(millis());
@@ -14,13 +21,52 @@ void setup() {
     
     effects = new EffectsList();
     effects->setEffect(0);
+
+    // КНОПКА
+    touch.setStepTimeout(BUTTON_STEP_TIMEOUT);
+    touch.setClickTimeout(BUTTON_CLICK_TIMEOUT);
 }
 
 unsigned long tick = 0;
 int tps = 0;
+bool auto_mode = true;
+long int auto_mode_cnt = 0;
+
+
+void buttonTick() {
+    touch.tick();
+    uint8_t clickCount = touch.hasClicks() ? touch.getClicks() : 0U;
+    if (clickCount == 1U) {
+        if (auto_mode) {
+            auto_mode = false;
+            out("auto mode off");
+        } else {
+            effects->nextEffect();
+            out("next mode");
+        }
+    }
+    if (clickCount == 2U) {
+        if (!auto_mode) {
+            auto_mode = true;
+            auto_mode_cnt = millis();
+            out("auto mode on");
+        }
+    }
+}
+
+void auto_mode_tick() {
+    if (auto_mode && millis() - auto_mode_cnt > AUTOMOD_INTERVAL) {
+        auto_mode_cnt = millis();
+        effects->nextEffect();
+        out("auto next mode");
+    }
+}
 
 void loop() {
     effects->onTick();
+
+    buttonTick();
+    auto_mode_tick();
     
     tps++;
     if (millis() > tick * 1000 ) {
