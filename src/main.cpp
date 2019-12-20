aafe1fdca366971b76c559b6afdc37eb555af384
npo6ka <npo6ka100@gmail.com>
2019-12-21 00:32:02 +0300

    fix 10 mod

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void setup() {
     setup_buttons();
 
     FastLED.setBrightness(150);
-    EffectsList::getInstance().setEffect(16);
+    EffectsList::getInstance().setEffect(10);
 }
 
 //unsigned long tick = 0;
