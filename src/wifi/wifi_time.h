1d684c14feed82336c66aabbe5d0ee86c3e5ee6a
AHTOH108 <ivanov.92@mail.ru>
2022-12-11 22:46:52 +0300

    test witi time

--- src/wifi/wifi_time.h
+++ src/wifi/wifi_time.h
@@ -1,8 +1,8 @@
 #pragma once
 
-// #include "NTPClient.h"
-#include "ESP8266WiFi.h"
-#include "ESP8266WebServer.h"
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
 #define SSID "Keenetic-108_2.4"     // для внешней точки доступа
 #define PASS "lost4815162342"       // для внешней точки доступа
@@ -10,25 +10,30 @@
 // #define AP_SSID "Test_WiFi_NodeMCU" // для wifi на плате
 // #define AP_PASS "12345678"          // для wifi на плате
 
-unsigned long tick_size = 1000;         // Частота срабатывания метода onTick(); в мс
+unsigned long tick_size = 5000;         // Частота срабатывания метода onTick(); в мс
 unsigned long prev_micros_wifi_tick = 0;// последнее время срабатывания метода onTickWiFi()
+
+unsigned long utcOffsetInSeconds = 3600;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+long time_this = 0;
+
+// Определение NTP-клиента для получения времени
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 // Вызвать этот метод в main setup()
 void setupWiFi() { 
-    // if (!tryConnectToRouter()){
-    //     Serial.print(" WiFi not found: ");
-    //     Serial.print(SSID);
-    //     Serial.println(". Starting your wifi...");
-    //     startOwnWiFi();
-    // }else{
-    //     Serial.println(" WiFi is successfully connected");
-    //     IPAddress myIP = WiFi.localIP();
-    //     Serial.print("Access point Mode");
-    //     Serial.print("AP IP address: ");
-    //     Serial.println(myIP);
-    // }
-    
-    // server_init();
+
+    WiFi.begin(SSID, PASS);
+
+    while ( WiFi.status() != WL_CONNECTED ){
+        delay(1000);
+        Serial.print(".");
+    }
+
+    timeClient.begin();
 }
 
 bool try_connect(){
@@ -40,7 +45,30 @@ bool try_connect(){
 void tick_wifi(){
     if (millis() - prev_micros_wifi_tick > tick_size){
         prev_micros_wifi_tick = millis();
-        out("wifi_timer::tick_wifi()   millis = %d\n", prev_micros_wifi_tick);
+        
+        if (time_this < 10000){
+            Serial.println("time_this < 10000");
+            time_this = timeClient.getEpochTime();
+        }else{
+            Serial.println("time_this >= 10000");
+            time_this = time_this + tick_size;
+        }
+
+        timeClient.update();
+        Serial.print(daysOfTheWeek[timeClient.getDay()]);
+        Serial.print(", ");
+        Serial.print(timeClient.getHours());
+        Serial.print(":");
+        Serial.print(timeClient.getMinutes());
+        Serial.print(":");
+        Serial.println(timeClient.getSeconds());
+                
+        Serial.print("delta = ");
+        long r = timeClient.getEpochTime();
+        
+        Serial.println(r);
+        r = r - time_this;
+        Serial.println(r);
     }
 }
 
