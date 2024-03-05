c1a3604bd3d4f2489e99b4dd77429ef0ae797646
npo6ka <lenchik.102@mail.ru>
2024-03-05 22:55:28 +0300

    small fixes

--- src/libs/debug_lib.h
+++ src/libs/debug_lib.h
@@ -3,7 +3,7 @@
 #if DEBUG
   #include <SoftwareSerial.h>
 
-  void static debug_setup()
+  inline void debug_setup()
   {
     Serial.begin(115200);
   }
@@ -17,7 +17,7 @@
    *    отрицательное чило - нехватка памяти, модуль числа равен запрашиваемой памяти
    *    положительное число - количество символов, выведенное в Serial
    */
-  const static size_t out(const char *szFormat, ...)
+  inline const size_t out(const char *szFormat, ...)
   {
     va_list argptr;
     va_start(argptr, szFormat);
