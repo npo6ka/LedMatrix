d927dfb0e6548cbde7f564a4336a39331c12e311
npo6ka <lenchik.102@mail.ru>
2024-12-18 00:48:49 +0300

    rafctoring observer

--- src/events/observer.h
+++ src/events/observer.h
@@ -1,63 +1,127 @@
 #include "stdint.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include <utility>
 
-class IObservableModeList {
-public:
-    virtual void eventNextMode() = 0;
-    virtual void eventPrevMode() = 0;
+template <typename... Args> void out(Args... args) { printf(args...); }
+
+// https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0%D1%82%D0%B5%D0%BB%D1%8C_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)
+
+enum class EventType {
+  ChangeAutoMod = 0,
+  ChangeMode,
+  ChangePowerState,
+  ChangeBrightness,
+  ChangeSpeed,
+  ChangeModVar,
+  EventAmount // используется, чтобы знать сколько всего ивентов
 };
 
-class IObservableAutomode {
+// если хотим получать уведомления об ивентах, наследуемся от этого класса и
+// реализуем метод handleEvent
+class IObserver {
 public:
-    virtual void eventAutomodeSet(bool val) = 0;
-    virtual void eventAutomodeSwitch() = 0;
+  virtual void handleEvent(EventType EventType) = 0;
 };
 
-class IObservablePower {
-public:
-    virtual void eventPowerSet(bool val) = 0;
-    virtual void eventPowerSwitch() = 0;
-};
+class Observable {
+  std::pair<int, IObserver **> *observerList;
 
-class IObservableMode {
-public:
-    virtual void eventBrightnessSet(int val) = 0;
-    virtual void eventBrightnessIncrement(int val) = 0;
-    virtual void eventBrightnessReduction(int val) = 0;
-    virtual void eventSpeedSet(int val) = 0;
-    virtual void eventSpeedIncrement(int val) = 0;
-    virtual void eventSpeedReduction(int val) = 0;
-    virtual void eventModeSetVal(uint8_t argId, uint64_t val) = 0;
-};
+  // проверка корретного типа и возврат id в виде int
+  // 0 при ошибке
+  int type_to_int(EventType etype) {
+    if (etype >= EventType::ChangeAutoMod && etype < EventType::EventAmount) {
+      return static_cast<int>(etype);
+    } else {
+      out("Error check EventType: out of range\n");
+      return 0;
+    }
+  }
 
-class Observer {
-    IObservableModeList *modeList = nullptr;
-    IObservableAutomode *automode = nullptr;
-    IObservablePower *power = nullptr;
-    IObservableMode *curMode = nullptr;
+  Observable() {
+    int eventAmount = static_cast<int>(EventType::EventAmount);
+    observerList = new std::pair<int, IObserver **>[eventAmount];
+    for (int i = 0; i < eventAmount; i++) {
+      observerList[i].first = 0;
+      observerList[i].second = nullptr;
+    }
+  }
+
+  ~Observable() {
+    int eventAmount = static_cast<int>(EventType::EventAmount);
+    for (int i = 0; i < eventAmount; ++i) {
+      if (observerList[i].second) {
+        free(observerList[i].second);
+      }
+    }
+    free(observerList);
+  }
+
+  Observable(const Observable &) = delete;
+  Observable &operator=(const Observable &) = delete;
+
 public:
-    Observer() {}
-    ~Observer() {}
-    void subscribeModeListEvents(IObservableModeList *inst);
-    void subscribeAutomodeEvents(IObservableAutomode *inst);
-    void subscribePowerEvents(IObservablePower *inst);
-    void subscribeCurModeEvents(IObservableMode *inst);
-// Event notification system
-// -----------IObservableModeList-----------
-    void notityNextMode();
-    void notityPrevMode();
-// -----------IObservableAutomode-----------
-    void notityAutomodeSet(bool val);
-    void notityAutomodeSwitch();
-// -----------IObservablePower-----------
-    void notityPowerSet(bool val);
-    void notityPowerSwitch();
-// -----------IObservableMode-----------
-    void notityBrightnessSet(int val);
-    void notityBrightnessIncrement(int val);
-    void notityBrightnessReduction(int val);
-    void notitySpeedSet(int val);
-    void notitySpeedIncrement(int val);
-    void notitySpeedReduction(int val);
-    template<class T>
-    void notityModeSetVal(uint8_t argId, T val);
+  static Observable &instance() {
+    static Observable singleton;
+    return singleton;
+  }
+
+  void addObserver(EventType etype, IObserver *observer) {
+    int type = type_to_int(etype);
+    // ищем пустую ячейку для записи
+    int empty_location = -1;
+    int observer_location = -1;
+    for (int i = 0; i < observerList[type].first; ++i) {
+      if (observerList[type].second[i] == nullptr) {
+        empty_location = i;
+      }
+      if (observerList[type].second[i] == observer) {
+        observer_location = i;
+        break;
+      }
+    }
+    if (observer_location) {
+      out("Error addObserver: observer exists on list");
+      return;
+    }
+    // если не нашли пустую ячейку расширяем память
+    if (empty_location == -1) {
+      observerList[type].first++;
+      observerList[type].second =
+          (IObserver **)realloc(observerList[type].second,
+                                sizeof(IObserver *) * observerList[type].first);
+      observerList[type].second[observerList[type].first - 1] = observer;
+    } else {
+      // если нашли пустую ячейку, то записываем туда
+      observerList[type].second[empty_location] = observer;
+    }
+  }
+
+  void removeObserver(EventType etype, IObserver *observer) {
+    int type = type_to_int(etype);
+    int observer_location = -1;
+
+    for (int i = 0; i < observerList[type].first; ++i) {
+      if (observerList[type].second[i] == observer) {
+        observer_location = i;
+        break;
+      }
+    }
+
+    if (observer_location != -1) {
+      observerList[type].second[observer_location] = nullptr;
+    } else {
+      out("Error removeObserver: EventType not found\n");
+    }
+  }
+
+  void notifyObserver(EventType etype) {
+    int type = type_to_int(etype);
+
+    for (int i = 0; i < observerList[type].first; ++i) {
+      if (observerList[type].second[i]) {
+        observerList[type].second[i]->handleEvent(etype);
+      }
+    }
+  }
 };
