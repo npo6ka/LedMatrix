658f20dceb1437a7847d3619426b7bad58c49c0f
npo6ka <npo6ka100@gmail.com>
2026-08-14 22:33:32 +0300

    fix ny2020

--- src/effects/effects_impl/ny2020.cpp
+++ src/effects/effects_impl/ny2020.cpp
@@ -11,7 +11,7 @@
 #define Y 0xffff00
 #define C 0x00ffff
 #define W 0xffffff
-#define BR 0x402010
+#define NY_BR 0x402010
 #define LB 0xc08040
 #define O 0xff8000
 #define P 0xffc0c0
@@ -22,10 +22,10 @@ static constexpr uint32_t sprite1[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
     0, 0, W, DB, DB, DB, W, DR, 0, 0,
     0, W, DB, DB, DB, DB, DB, W, 0, 0,
     W, DB, DB, DB, DB, DB, DB, DB, W, 0,
-    0, BR, BR, BR, BR, BR, BR, BR, 0, 0,
-    0, BR, Y, Y,   BR, Y, Y,   BR, 0, 0,
-    0, BR, Y, Y,   BR, Y, Y,   BR, 0, 0,
-    0, BR, BR, BR, BR, Y, Y,   BR, 0, 0,
+    0, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, 0, 0,
+    0, NY_BR, Y, Y,   NY_BR, Y, Y,   NY_BR, 0, 0,
+    0, NY_BR, Y, Y,   NY_BR, Y, Y,   NY_BR, 0, 0,
+    0, NY_BR, NY_BR, NY_BR, NY_BR, Y, Y,   NY_BR, 0, 0,
     W, W,  W,  W,  W,  Y, Y,   W, W, W,
 };
 
@@ -34,11 +34,11 @@ static constexpr uint32_t sprite2[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
     0, 0,  0,  W,  DB, W,  0,  0,  0,  0,
     0, 0,  W,  DB, DB, DB, W,  0,  0,  0,
     0, W,  DB, DB, DB, DB, DB, W,  0,  0,
-    0, 0,  BR, BR, BR, BR, BR, 0,  0,  0,
-    0, 0,  BR, C,  C,  C,  BR, 0,  0,  0,
-    0, 0,  BR, C,  C,  C,  BR, 0,  0,  0,
-    0, 0,  BR, BR, BR, BR, BR, 0,  0,  0,
-    0, W,  BR, BR, BR, BR, BR, W,  W,  0,
+    0, 0,  NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, 0,  0,  0,
+    0, 0,  NY_BR, C,  C,  C,  NY_BR, 0,  0,  0,
+    0, 0,  NY_BR, C,  C,  C,  NY_BR, 0,  0,  0,
+    0, 0,  NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, 0,  0,  0,
+    0, W,  NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, W,  W,  0,
     W, W,  W,  W,  W,  W,  W,  W,  W,  W,
 };
 
@@ -122,15 +122,15 @@ static constexpr uint32_t sprite8[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
 
 static constexpr uint32_t sprite9[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0, BR,  0,  0,  0,  0,  0,  0,
-    0,  0, BR, BR,  0,  0,  0,  0,  0,  0,
+    0,  0,  0, NY_BR,  0,  0,  0,  0,  0,  0,
+    0,  0, NY_BR, NY_BR,  0,  0,  0,  0,  0,  0,
     0, LB, LB,  0,  0,  0,  0,  0,  0,  0,
-   BR, LB, LB,  0,  0,  0,  0,  0,  0,  0,
+   NY_BR, LB, LB,  0,  0,  0,  0,  0,  0,  0,
     0,  0, LB, LB, LB, LB, LB, LB, LB,  0,
     0,  0, LB, LB, LB, LB, LB, LB,  0,  0,
     0, LB, LB, LB, LB, LB, LB, LB,  0,  0,
-    0, BR,  0, LB,  0, LB,  0, LB,  0,  0,
-    0,  0,  0, BR,  0, BR,  0, BR,  0,  0,
+    0, NY_BR,  0, LB,  0, LB,  0, LB,  0,  0,
+    0,  0,  0, NY_BR,  0, NY_BR,  0, NY_BR,  0,  0,
 };
 
 static constexpr uint32_t sprite10[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
@@ -147,8 +147,8 @@ static constexpr uint32_t sprite10[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
 };
 
 static constexpr uint32_t sprite11[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
-    0,  0,  0, BR, BR, BR,  0,  0,  0,  0,
-    0, BR, BR, BR, BR, BR, BR, BR,  0,  0,
+    0,  0,  0, NY_BR, NY_BR, NY_BR,  0,  0,  0,  0,
+    0, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR,  0,  0,
     0,  0,  W,  W,  W,  W,  W,  0,  0,  0,
     0,  W,  W,  0,  W,  0,  W,  W,  0,  0,
     0,  0,  W,  W,  W,  W,  W,  0,  0,  0,
@@ -173,7 +173,7 @@ static constexpr uint32_t sprite12[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
 };
 
 static constexpr uint32_t sprite13[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
-    0,  0,  0,  0, BR, BR,  0,  0,  0,  0,
+    0,  0,  0,  0, NY_BR, NY_BR,  0,  0,  0,  0,
     0,  0,  0,  0,  G,  G,  0,  0,  0,  0,
     0,  0,  0,  0,  G,  G,  0,  0,  0,  0,
     0,  0,  0,  0,  G,  G,  0,  0,  0,  0,
@@ -193,9 +193,9 @@ static constexpr uint32_t sprite14[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
     0,  0,  0,  0,  0,  R, DR,  0,  0,  0,
     0,  0,  P,  R,  R, DR, DR,  0,  0,  R,
     0,  0,  0,  0, DR, DR, DR,  0,  R,  R,
-   BR,  0,  0, DR, DR, DR, DR,  R,  R,  R,
-   BR,  0,  R,  R,  R,  R,  R,  R,  R,  R,
-    0, BR, BR, BR, BR, BR, BR, BR, BR, BR,
+   NY_BR,  0,  0, DR, DR, DR, DR,  R,  R,  R,
+   NY_BR,  0,  R,  R,  R,  R,  R,  R,  R,  R,
+    0, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR,
 };
 
 static const uint32_t sprite15[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
@@ -230,11 +230,11 @@ static constexpr uint32_t sprite17[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
     0,  0,  0,  W,  W,  W,  0,  0,  0,  0,
     0,  0,  0,  W,  W,  W,  0,  0,  0,  0,
     0,  0,  W,  W,  W,  W,  W,  0,  0,  0,
-    0,  0, BR,  W, BR,  W, BR,  0,  0,  0,
-    0, BR, BR,  W, BR,  W, BR, BR,  0,  0,
-    0, BR, BR, BR, BR,  W, BR, BR,  0,  0,
-   BR, BR, BR, BR, BR, BR, BR, BR, BR,  0,
-   BR, BR, BR, BR, BR, BR, BR, BR, BR,  0,
+    0,  0, NY_BR,  W, NY_BR,  W, NY_BR,  0,  0,  0,
+    0, NY_BR, NY_BR,  W, NY_BR,  W, NY_BR, NY_BR,  0,  0,
+    0, NY_BR, NY_BR, NY_BR, NY_BR,  W, NY_BR, NY_BR,  0,  0,
+   NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR,  0,
+   NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR, NY_BR,  0,
 };
 
 static constexpr uint32_t sprite18[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
@@ -272,7 +272,7 @@ static constexpr uint32_t sprite19[NY_SPRITE_W*NY_SPRITE_H] PROGMEM = {
 #undef Y
 #undef C
 #undef W
-#undef BR
+#undef NY_BR
 #undef LB
 #undef O
 #undef P
